f7a316a5352ea75fc549d30cfaacc4c7a10615f4
DmIl1289 <id1289@yandex.ru>
2023-04-16 12:21:06 +0300

    Initial commit

---
+++ src/main/java/com/example/testmultiplereposgithub/TestMultipleReposGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmultiplereposgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMultipleReposGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMultipleReposGithubApplication.class, args);
+    }
+
+}
